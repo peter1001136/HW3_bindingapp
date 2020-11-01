2eae38476508b735d492107b3f3c225644c8ad06
Peterchen <peter1001136@gmail.com>
2020-11-01 16:40:31 +0800

    Initial Commit

---
+++ HW3_bindingapp/HW3_bindingappApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_bindingappApp.swift
+//  HW3_bindingapp
+//
+//  Created by 陳政沂 on 2020/11/1.
+//
+
+import SwiftUI
+
+@main
+struct HW3_bindingappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
